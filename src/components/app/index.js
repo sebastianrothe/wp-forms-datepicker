85e397162f2cef1857f4355ca2233e71c34e24ff
Sebastian Rothe <rothe@infai.org>
2019-02-11 23:20:46 +0100

    remove daypicker

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,16 +1,12 @@
 import { Component } from 'inferno';
 
 import MyDatePicker from "../datepicker";
-import MyDayPicker from "../day-picker";
+//import MyDayPicker from "../day-picker";
 
 export default class App extends Component {
     render(props) {
       return (
-        <div>
-          <MyDatePicker />
-          <br />
-          <MyDayPicker />
-        </div>
+        <MyDatePicker />
       );
     }
   }
