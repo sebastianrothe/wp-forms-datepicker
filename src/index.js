33c8d4b9912c95ff355132886f0de5fbf352501a
Sebastian Rothe <rothe@infai.org>
2019-02-11 21:15:36 +0100

    add Fragment fallback

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ let poly = require("preact-cli/lib/lib/webpack/polyfills");
 import { h } from "preact";
 import habitat from "preact-habitat";
 
+import React, { Fragment } from 'react'
+React.Fragment = 'x-fragment'
+
 import App from "./components/app";
 
 let _habitat = habitat(App);
